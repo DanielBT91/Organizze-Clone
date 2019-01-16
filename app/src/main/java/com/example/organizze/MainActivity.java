cedc1d0a70a8c40cb125a81c13f6925561b2a6cd
Lucas <lucasjoao85@gmail.com>
2019-01-16 14:08:20 -0200

    second commit 16/01/2019

--- app/src/main/java/com/example/organizze/MainActivity.java
+++ app/src/main/java/com/example/organizze/MainActivity.java
@@ -1,13 +1,67 @@
 package com.example.organizze;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.organizze.Activity.CadastroActivity;
+import com.example.organizze.Activity.LoginActivity;
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
+public class MainActivity extends IntroActivity {
+    private Button btnCadastro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+
+
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new FragmentSlide.Builder()
+            .fragment(R.layout.intro_1)
+                .background(android.R.color.white)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .fragment(R.layout.intro_2)
+                .background(android.R.color.white)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .fragment(R.layout.intro_3)
+                .background(android.R.color.white)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .fragment(R.layout.intro_4)
+                .background(android.R.color.white)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .fragment(R.layout.intro_cadastro)
+                .background(android.R.color.white)
+                .canGoForward(false)
+                .build()
+        );
+
     }
+
+    public void onClickCadastro(View view){
+        startActivity(new Intent(this, CadastroActivity.class));
+    }
+
+    public void onClickLogin(View vew){
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
 }
